32686520f2289d530bd046aca10c206233d9f6e6
Sachiko Mizutori <sachikomizutori@SachikonoMacBook-Air.local>
2020-06-15 10:23:53 +0900

    change mix to asset

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -5,8 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-    <link href="{{ mix('css/style.css') }}" rel="stylesheet">
-    <script src="{{ mix('js/tinymce/tinymce.min.js') }}"></script>
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <script src="{{ asset('js/tinymce/tinymce.min.js') }}"></script>
     <script>tinymce.init({ selector:'textarea', language: "ja", toolbar: 'insert', plugins: "image link table", insert_button_items: 'image link | inserttable' });</script>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
